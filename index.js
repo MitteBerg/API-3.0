f7485c5fbbcb6c234219dcc05ad042f8f154a7a7
hoangclem <tranhoang092001@gmail.com>
2022-07-26 17:24:48 +0700

    first version

---
+++ index.js
@@ -0,0 +1,26 @@
+// import content of a.env
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+//connect to the mongodb
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+//import routes 
+const routes = require('./routes/routes');
+app.use('/api', routes)
