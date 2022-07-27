aeba13f182c70fab141ed5268b0061a3b7f663eb
hoangclem <tranhoang092001@gmail.com>
2022-07-27 10:09:44 +0700

    problem with the model.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// import content of a.env
 require('dotenv').config();
 const express = require('express');
+
+// import content of a.env
+
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
+
 //connect to the mongodb
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -19,7 +22,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    console.log(`Server Started at http://localhost:${3000}`)
 })
 //import routes 
 const routes = require('./routes/routes');
--- routes/routes.js
+++ routes/routes.js
@@ -4,9 +4,21 @@ const router = express.Router()
 
 module.exports = router;
 
+
 //Post Method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 
 //Get all Method
@@ -16,7 +28,7 @@ router.get('/getAll', (req, res) => {
 
 //Get by ID Method
 router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
+    res.send(req.params.id)
 })
 
 //Update by ID Method
@@ -29,7 +41,5 @@ router.delete('/delete/:id', (req, res) => {
     res.send('Delete by ID API')
 })
 
-//Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
-})
+//import the model
+const Model = require('../models/model');
